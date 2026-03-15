7e6895897457d278110da9f064e01ecdfc1722fa
GAMERXTAKJELAS <syamsulock0457@gmail.com>
2026-03-15 23:41:23 +0800

    Add jobsheet9 HTML, CSS, and JS files for shopping cart functionality with responsive design

---
+++ syamsul/script/jobsheet9lab.js
@@ -0,0 +1,11 @@
+// First array: image filenames
+var filenames = ["106020.jpg", "116010.jpg", "120010.jpg"]; 
+
+// Second array: product titles
+var titles = ["Girl with a Pearl Earring", "Artist Holding a Thistle", "Portrait of Eleanor of Toledo"];
+
+// Third array: quantities purchased
+var quantities = [3, 1, 2];
+
+// Fourth array: individual prices
+var prices = [80, 125, 75];
