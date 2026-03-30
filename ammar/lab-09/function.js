0d2f9880d9cf4548c6c7f13c01545e881669b563
amru0920 <muhdammar758@gmail.com>
2026-03-30 11:32:29 +0800

    Refactor Lab 9 files for improved readability and consistency; optimize function definitions and update HTML structure

--- ammar/lab-09/function.js
+++ ammar/lab-09/function.js
@@ -1,4 +1,3 @@
-/* define functions here */
 function outputCartRow(file, title, quantity, price, total) {
     document.write('<tr>');
     document.write('<td><img src="images/' + file + '"></td>');
@@ -9,26 +8,11 @@ function outputCartRow(file, title, quantity, price, total) {
     document.write('</tr>');
 }
 
-function calculateTotal(quantity, price) {
-    return quantity * price;
-}
-
-function calculateTax(subtotal, rate) {
-    return subtotal * rate;
-}
-
+function calculateTotal(quantity, price) { return quantity * price; }
+function calculateTax(subtotal, rate) { return subtotal * rate; }
 function calculateShipping(subtotal, threshold) {
-    if (subtotal > threshold) {
-        return 0;
-    } else {
-        return 40;
-    }
+    if (subtotal > threshold) { return 0; }
+    else { return 40; }
 }
-
-function calculateGrandTotal(subtotal, tax, shipping) {
-    return subtotal + tax + shipping;
-}
-
-function outputCurrency(num) {
-    document.write("$" + num.toFixed(2));
-}
+function calculateGrandTotal(subtotal, tax, shipping) { return subtotal + tax + shipping; }
+function outputCurrency(num) { document.write("$" + num.toFixed(2)); }
